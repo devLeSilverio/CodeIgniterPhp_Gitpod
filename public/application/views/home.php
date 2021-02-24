4b8d47a6a4f420940cf19469bb0bf2ddb7a05f1a
devLeSilverio <leticia.silverio09@gmail.com>
2021-02-24 17:30:28 +0000

    Creating a new controler

---
+++ public/application/views/home.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home - Site PW3</title>
+</head>
+<body>
+	<h1>Ola mundo!</h1>
+</body>
+</html>
