b80ac6f744529402fde1268d1972b0d09eee8421
devLeSilverio <leticia.silverio09@gmail.com>
2021-02-25 00:37:49 +0000

    Starting the front-end

--- public/application/views/home.php
+++ public/application/views/home.php
@@ -8,8 +8,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home - Site PW3</title>
+	<link rel="stylesheet" href="<?= base_url() ?>assets/reset.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/principal.css">
 </head>
 <body>
-	<h1>Ola mundo!</h1>
+	<div class="container">
+		<div class="row mt-4">
+			<div class="col-12">
+				<div class="jumbotron">
+					<h1 class="display-4">Ah,não sei.</h1>
+					<p class="lead">Não beba Monóxido de di-hidrogênio!!😘</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<nav class="navbar navbar-light bg-light">
+					<a class="navbar-brand">Menu</a>
+						<form class="form-inline">
+							<input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+							<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ir</button>
+						</form>
+				</nav>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-6">
+				<center><h4>No sei cara</h4></center>
+				<center><img  src="<?= base_url() ?>/assets/imgs/water.jpg" alt="agua" class="img-fluid" /></center>
+				<center><p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+				</p></center>
+				
+			</div>
+			<div class="col-6">
+				<center><h4>No sei cara</h4></center>
+				<center><img  src="<?= base_url() ?>/assets/imgs/water.jpg" alt="agua" class="img-fluid"/></center>
+				<center><p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+				</p></center>
+				
+			</div>
+		</div>
+	</div>
 </body>
 </html>
